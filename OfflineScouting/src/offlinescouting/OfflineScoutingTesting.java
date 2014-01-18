7f80b77634867dd9c87a33b99131b28a486473e5
ambether <barbequedcinders@gmail.com>
2014-01-18 15:19:00 -0800

    init
    1

---
+++ OfflineScouting/src/offlinescouting/OfflineScoutingTesting.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package offlinescouting;
+
+/**
+ *
+ * @author Robotics SE
+ */
+
+public class OfflineScoutingTesting {
+
+    public static void init(){
+        ScoutingWindowTest mainWindow = new ScoutingWindowTest();
+        mainWindow.setVisible(true);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        init();
+    }
+    
+}
